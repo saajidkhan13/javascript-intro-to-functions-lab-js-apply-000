f88de68bc692cdfdec33670ed19c6abc6941018a
Saajid Khan <saajid.khan13@stjohns.edu>
2018-08-04 16:44:50 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,6 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logShout(; {
- return(string.toUpperCase)
+function logShout(string) {
+ return console.log(string.toUpperCase)
 }
