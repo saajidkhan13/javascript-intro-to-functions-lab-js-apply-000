56d37e39c055bbac2f7dc1a1a8184c704533a48c
Saajid Khan <saajid.khan13@stjohns.edu>
2018-08-04 17:27:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function sayHiToGrandma(string) {
     return "I can't hear you";
   if (string.toUpperCase()) === string) {
     return "YES INDEED";
-  if (string.)
+  if (string === "I love you, Grandma")
   }
   }
 }
